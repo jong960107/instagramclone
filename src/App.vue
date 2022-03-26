74a3022e16ff09b7c3b1476b206611b7fdc4443b
jong960107 <jong960107@naver.com>
2022-03-27 06:46:44 +0900

    [#2]인스타그램 로그인창 만들기

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,26 @@
-<template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+<template lang="">
+  <div id="body">
+
+  <Section></Section>
+  <Footer></Footer>
+      </div>
+
 </template>
+<script>
+import Section from './components/signin/SectionComponent.vue'
+import Footer from './components/signin/FooterComponent.vue'
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+export default {
+  
+  components:{
+      Section,
+      Footer
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
   }
+
+
 }
+</script>
+<style lang="">
+  
 </style>
---
+++ src/components/signin/SectionComponent.vue
@@ -0,0 +1,28 @@
+<template lang="">
+    <div id="mainWrapper">
+      <div id="sectionImg">
+        <div id="images">
+          <img src="image1" alt="">
+          <img src="image2" alt="">
+          <img src="image3" alt="">
+        </div>
+    </div>
+        <div id="section">
+        <div id="signInWrapper">
+          <h1>Instagram</h1>
+          <input type="text" @model="username" placeholder="전화번호,사용자이름,이메일">
+          <input type="text" @model="password" placeholder="비밀번호">
+          <button type="submit">로그인</button>
+          <div id="facebookLogin"></div>
+        </div>
+        </div>
+        </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
